c4db4cf4e723ebd22afb665797d4f081e68474bb
Steven L. Speek <slspeek@gmail.com>
2015-08-30 20:33:23 +0200

    no dependency at gae_testing anymore

--- gae-e2e/src/functionalTest/java/com/googlecode/simpleblobstore/gae/GaeGuiceBerryEnv.java
+++ gae-e2e/src/functionalTest/java/com/googlecode/simpleblobstore/gae/GaeGuiceBerryEnv.java
@@ -1,14 +1,12 @@
 package com.googlecode.simpleblobstore.gae;
 
 import com.google.guiceberry.GuiceBerryModule;
-import com.google.guiceberry.TestWrapper;
 
 public class GaeGuiceBerryEnv extends GuiceBerryModule {
 
-    @Override
-    protected void configure() {
-        super.configure();
-        install(new GaeSimpleBlobstoreModule());
-        bind(TestWrapper.class).to(GaeLocalBlobstoreHelperTestWrapper.class);
-    }
+	@Override
+	protected void configure() {
+		super.configure();
+		install(new GaeSimpleBlobstoreModule());
+	}
 }
--- gae-e2e/src/main/java/com/googlecode/simpleblobstore/testserver/GaeGuiceServletConfig.java
+++ gae-e2e/src/main/java/com/googlecode/simpleblobstore/testserver/GaeGuiceServletConfig.java
@@ -24,11 +24,8 @@
 
 package com.googlecode.simpleblobstore.testserver;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.logging.Logger;
 
-import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.servlet.GuiceServletContextListener;
@@ -39,12 +36,11 @@ public class GaeGuiceServletConfig extends GuiceServletContextListener {
 	@Override
 	protected Injector getInjector() {
 		Injector i = Guice.createInjector(new GaeSimpleBlobstoreModule(),
-				new TestServletModule(), new TestModule());
+				new TestServletModule());
 		return i;
 	}
 
 	private class TestServletModule extends ServletModule {
-
 		@Override
 		protected void configureServlets() {
 			super.configureServlets();
@@ -56,14 +52,4 @@ public class GaeGuiceServletConfig extends GuiceServletContextListener {
 			serve("/info").with(InfoServlet.class);
 		}
 	}
-
-	private class TestModule extends AbstractModule {
-
-		@Override
-		protected void configure() {
-			bind(List.class).toInstance(new ArrayList<String>());
-		}
-
-	}
-
 }
